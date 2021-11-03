d9c002057c7872151b3441cf77e023704518da6d
IcaHuang <ica.huang@qq.com>
2021-11-03 23:45:56 +0800

    Add a package creationalpatterns. And add Singleton.java into it

--- creationalpatterns/Singleton.java
+++ creationalpatterns/Singleton.java
@@ -1,9 +1,9 @@
-//package CreationalPatterns;
+package creationalpatterns;
 
 public class Singleton {
     // lazy construction
     // the class variable is null if no instance is instantiated
-    private static ExampleSingleton uniqueInstance = null;
+    private static Singleton uniqueInstance = null;
 
     private int count;
     private Singleton() {
@@ -17,7 +17,7 @@ public class Singleton {
     }
 
     // lazy construction of the instance
-    public static ExampleSingleton getInstance() {
+    public static Singleton getInstance() {
         if (uniqueInstance == null) {
             uniqueInstance = new Singleton();
         }
@@ -25,10 +25,10 @@ public class Singleton {
     }
 
     public static void main(String[] args) {
-        ExampleSingleton singletonInstance1 = ExampleSingleton.getInstance();
+        Singleton singletonInstance1 = Singleton.getInstance();
         // Output: 1
         System.out.println(singletonInstance1.getCount());
-        ExampleSingleton singletonInstance2 = ExampleSingleton.getInstance();
+        Singleton singletonInstance2 = Singleton.getInstance();
         singletonInstance2.addCount();
 
         // Output: 2
